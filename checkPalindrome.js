7a8ed0500c4726856a50eb07c3a666bd435fce14
Steven Galvis <steven.galvis@gmail.com>
2022-10-24 20:49:31 -0400

    logging array to string

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -1,3 +1,17 @@
 function checkPalindrome(inputString) {
+  console.log(inputString);
+strArr = inputString.split('');
+console.log(strArr);
+  let reverse = [];
+for (let i = strArr.length-1; i >= 0; i--) {
+  console.log(strArr[i]);
+
+  reverse.push(strArr[i]);
+}
+let reverseStr = reverse.toString(' ');
+console.log(reverseStr);
+
 
 }
+
+checkPalindrome('noon');
