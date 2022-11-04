5db79c1cee0e1fbd421fe95f649d71899022ad56
steven galvis <steven.galvis@gmail.com>
2022-11-04 19:33:47 -0400

    getting highest product

--- adjacentElementsProduct.js
+++ adjacentElementsProduct.js
@@ -1,6 +1,21 @@
 function adjacentElementsProduct(inputArray) {
     //Given an array of integers, find the pair of adjacent elements
     //that has the largest product and return that product.
+    console.log(inputArray);
+    //find the largest two nums
+    let highestArr = [];
+    let highestNum = inputArray[0];
+    let product = 0;
 
+    for (let i = 0; i < inputArray.length; i++) {
+
+      if (inputArray[i] > highestNum) {
+        console.log(inputArray[i]);
+        highestNum = inputArray[i];
+        product = highestNum * inputArray[i];
+      }
+    }
+    console.log(product);
 }
-inputArray = [3, 6, -2]
+inputArray = [3, 6, -2, -5, 7, 3];
+adjacentElementsProduct(inputArray);
