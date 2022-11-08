bb94cd07f340ca5b8f4568d54c0e690af7894927
steven galvis <steven.galvis@gmail.com>
2022-11-07 19:56:05 -0500

    solution for adjacentElementsProduct

--- adjacentElementsProduct.js
+++ adjacentElementsProduct.js
@@ -2,31 +2,28 @@ function adjacentElementsProduct(inputArray) {
     //Given an array of integers, find the pair of adjacent elements
     //that has the largest product and return that product.
     console.log(inputArray);
-    //find the largest two nums
-    let highestArr = [];
-    let highestNum = inputArray[0];
-    let product = 0;
+      //find the largest two nums
+      let highestArr = [];
+      let highestNum = inputArray[0];
+      let product = inputArray[0] * inputArray[1];
 
-    for (let i = 0; i < inputArray.length; i++) {
+      for (let i = 0; i < inputArray.length; i++) {
 
 
-      if (inputArray[i-1] * inputArray[i] > product) {
-        product = inputArray[i-1] * inputArray[i];
-      }
-
-      if   if (inputArray[i+1] * inputArray[i] > product) {
-          product = inputArray[i+1] * inputArray[i];
+        if (inputArray[i-1] * inputArray[i] > product) {
+          product = inputArray[i-1] * inputArray[i];
         }
 
+        if (inputArray[i+1] * inputArray[i] > product) {
+            product = inputArray[i+1] * inputArray[i];
+          }
 
 
-      if (inputArray[i] > highestNum) {
-        console.log(inputArray[i]);
-        highestNum = inputArray[i];
-        product = highestNum * inputArray[i];
+
       }
-    }
-    console.log(product);
+
+      console.log(product);
+      return product;
 }
-inputArray = [3, 6, -2, -5, 7, 3];
-adjacentElementsProduct(inputArrmm;
+inputArray = [-23, 4, -3, 8, -12];
+adjacentElementsProduct(inputArray);
